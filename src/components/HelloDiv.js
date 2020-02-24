3e6005be8dc36d3c8d8dfac5c28c72a56487966b
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-24 16:04:30 -0500

    Activity 03 week 19 solved.

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,5 +1,16 @@
-function HelloDiv() {
-  return null;
+import React from "react";
+
+const HelloDiv = () => {
+  return (
+    <div>
+      <h1>Hello, my name is Scott.</h1>
+      <ul>
+        <li>Reading</li>
+        <li>Coding</li>
+        <li>Music</li>
+      </ul>
+    </div>
+  );
 }
 
 export default HelloDiv;
