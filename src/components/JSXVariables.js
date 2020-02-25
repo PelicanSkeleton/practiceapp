a6af453cc89a442baa8d330fa976bd43e759b30d
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-24 19:19:50 -0500

    Activity 07 week 19 complete.

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -1,13 +1,28 @@
 import React from "react";
 
+let name = "Scott";
+const thoughts = "is intimidating but also exciting.";
+const NoVowels = string => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let result = "";
+
+  for(let i = 0; i < string.length; i++){
+    if(!vowels.includes(string[i].toLowerCase())) {
+      result += string[i];
+    }
+  }
+  return result;
+};
+
 function JSXVariables() {
   return (
     <div className="main-container">
       <div className="container">
         <div className="jumbotron">
-          <h1>Hi! My name is (insert name here)</h1>
-          <h2>My name has (insert number of letters in name here) letters</h2>
-          <h2>I think React (insert thoughts about React here)</h2>
+          <h1>Hi! My name is {name}</h1>
+          <h2>My name has {name.length} letters</h2>
+          <h2>I think React {thoughts}</h2>
+          <h2>My name without vowels is {NoVowels(name)}. </h2>
         </div>
       </div>
     </div>
