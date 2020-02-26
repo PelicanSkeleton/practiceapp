d596400a58ea93bdef01ed09e1717b81bf3acd4d
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-26 15:50:52 -0500

    Activity 13 week 19 complete with bonus.

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 function List(props) {
+  const notPurchased = props.groceries.filter(grocery => !grocery.purchased);
+
+  const listItems = props.groceries.map(grocery => (
+    <li key={grocery.id}>{grocery.name}</li>
+  ))
+
   return (
-    <ul className="list-group">
-      Using the map method, render one li element displaying the text property of each grocery
-      object.
-    </ul>
+    <div>
+      <h1>Grocery List</h1>
+
+      <ul className="list-group">{listItems}</ul>
+
+      <h1>Items Not Purchased</h1>
+
+      <ul className="list-group">{notPurchased.map(item => (
+        <li className="list-group-item" key={item.id}>
+          {item.name}
+        </li>
+      ))}
+      </ul>
+    </div >
   );
 }
 
