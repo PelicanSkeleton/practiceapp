ed4a6c67e59bb831c45be2d6c8bd30ed2b8da8dc
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-26 19:23:28 -0500

    Activity 18 week 19 start.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
-import "./App.css";
+import React from "react";
+import Form from "./components/Form";
 
-
-class App extends Component {
-  state = {
-    friends
-  };
-
-  removeFriend = id => {
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-
-    this.setState({ friends });
-  };
-
-  render() {
-    return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
-
+function App() {
+  return <Form />;
 }
 
 export default App;
